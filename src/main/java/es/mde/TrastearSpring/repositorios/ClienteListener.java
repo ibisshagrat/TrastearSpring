08808711e5d758b49931addb1659b74bd2505a76
NicoCI <ibisshagrat@hotmail.com>
2021-05-13 09:09:08 +0200

    Otro listener más, preRemove

--- src/main/java/es/mde/TrastearSpring/repositorios/ClienteListener.java
+++ src/main/java/es/mde/TrastearSpring/repositorios/ClienteListener.java
@@ -1,6 +1,7 @@
 package es.mde.TrastearSpring.repositorios;
 
 import javax.persistence.PrePersist;
+import javax.persistence.PreRemove;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -20,6 +21,11 @@ public class ClienteListener {
 	@PrePersist
 	public void preguardarCliente(ClienteVIP cliente) {
 		System.err.println("Voy a guardar el cliente: " + cliente.getNombre());
+	}
+	
+	@PreRemove
+	public void preBorrar(ClienteVIP cliente) {
+		System.err.println("Va a borrar el cliente: " + cliente.getNombre());
 	}
 	
 }
