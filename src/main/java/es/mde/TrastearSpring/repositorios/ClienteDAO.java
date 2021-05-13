914f087f282b76b186f827e4c15c35c2f7fb5f67
Nicolás Corada Ibáñez <ibisshagrat@hotmail.com>
2021-05-13 23:44:56 +0200

    Probando metodos JPA

--- src/main/java/es/mde/TrastearSpring/repositorios/ClienteDAO.java
+++ src/main/java/es/mde/TrastearSpring/repositorios/ClienteDAO.java
@@ -3,6 +3,7 @@ package es.mde.TrastearSpring.repositorios;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
@@ -12,6 +13,6 @@ import es.mde.TrastearSpring.entidades.Cliente;
 public interface ClienteDAO extends JpaRepository<Cliente, Long>, ClienteDAOCustom{
 
 	@RestResource(path="buscar")
-	List<Cliente> findByNombre(String nombre);
+	List<Cliente> findByNombreContainsIgnoreCase(@Param("nombre") String nombre);
 
 }
